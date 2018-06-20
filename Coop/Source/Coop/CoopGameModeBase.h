65475e2d0da2457675158c2813e95fc4bd22ca9c
terajoel <josejoeldejesus@gmail.com>
2018-06-20 07:01:08 -0500

    initial commit, empty project

---
+++ Coop/Source/Coop/CoopGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CoopGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COOP_API ACoopGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
