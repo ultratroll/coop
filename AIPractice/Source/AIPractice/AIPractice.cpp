cd931fcb71dd46d42ebbaebc16a414cbc705a064
Terajoel (Jose De Jesus) <jdejesus@teravisiongames.com>
2019-01-30 17:39:59 -0500

    Small project to practice AI, EQS, ect

---
+++ AIPractice/Source/AIPractice/AIPractice.cpp
@@ -0,0 +1,7 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "AIPractice.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AIPractice, "AIPractice" );
+ 
---
+++ AIPractice/Source/AIPractice/AIPracticeGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AIPracticeGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AAIPracticeGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AAIPracticeGameMode();
+};
+
+
+
